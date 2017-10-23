ba1e8501f9c4609f742d92b127844cdce44beb30
Lucas Romão Silva <lucas.romao.silva@gmail.com>
2017-10-23 17:24:39 -0200

    comecando tudo

---
+++ SonarUI/Projeto.java
@@ -0,0 +1,31 @@
+public class Projeto
+{
+	private int angle;
+	private int x;
+	private int y;
+
+	public void polarToCartesian(float m, float angle) {
+		return;
+	}
+
+	public void changeBasis(float x, float y, float angle) {
+		return; 
+	}
+
+	public void plotLines(float[] points) {
+		return;
+	}
+
+	public void readScans() {
+		return; 
+	}
+
+
+
+ 	public static void main(String[] args) {
+        
+        int dim = 10;
+
+   
+    }
+}
